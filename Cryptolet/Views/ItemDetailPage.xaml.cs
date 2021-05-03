61f6777e62649c170657927e5bf5c9e432eed841
Richard Hunter <r-hunter@live.co.uk>
2021-05-03 15:35:08 +0100

    Add project files.

---
+++ Cryptolet/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Cryptolet.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Cryptolet.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ Cryptolet/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Cryptolet.Models;
+using Cryptolet.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Cryptolet.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
